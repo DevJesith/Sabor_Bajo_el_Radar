1b3048224b4de62cba6cf95c25aaa7dc115409b9
Jesith Mejia <jesithmanuel@gmail.com>
2025-12-05 17:24:20 -0500

    refactor (Admin): Unify administrador logic a AdminController

--- Sabor_Bajo_el_Radar/src/main/java/com/sbr/sabor_bajo_el_radar/controller/DashboardViewController.java
+++ Sabor_Bajo_el_Radar/src/main/java/com/sbr/sabor_bajo_el_radar/controller/DashboardViewController.java
@@ -1,17 +1,31 @@
-package com.sbr.sabor_bajo_el_radar.controller;
-
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-
-@Controller
-@RequestMapping("/dashboard")
-public class DashboardViewController {
-
-    @GetMapping("/admin")
-    public String usuarios() {
-        return "Administrador/panel_administrador/panel_administrador";
-    }
-
-
-}
+//package com.sbr.sabor_bajo_el_radar.controller;
+//
+//import com.sbr.sabor_bajo_el_radar.model.Usuario;
+//import com.sbr.sabor_bajo_el_radar.repository.UsuarioRepository;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.stereotype.Controller;
+//import org.springframework.ui.Model;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//
+//import java.util.List;
+//
+//@Controller
+//@RequestMapping("/dashboard")
+//public class DashboardViewController {
+//
+//    @Autowired
+//    private UsuarioRepository usuarioRepository;
+//
+//    @GetMapping("/admin")
+//    public String usuarios(Model model) {
+//
+//        List<Usuario> usuarios = usuarioRepository.findAll();
+//
+//        model.addAttribute("usuarios", usuarios);
+//
+//        return "Administrador/panel_administrador/panel_administrador";
+//    }
+//
+//
+//}
